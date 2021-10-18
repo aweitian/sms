71c9931e6f34e38dbf7241e1cb61c816d0f069fa
awei.tian <awei.tian@qq.com>
2021-10-18 08:56:32 +0800

    unit test

--- tests/AliTest.php
+++ tests/AliTest.php
@@ -3,23 +3,23 @@
 
 class AliTest extends PHPUnit_Framework_TestCase
 {
-	/**
-	* A basic test example.
-	*
-	* @return void
-	*/
-	public function testx()
-	{
-		$test = new \Aw\Sms\Ali\Send('LTAI4********HwwtwPzTrF','Ph6JFTLC********GaGUw');
-        $test->setPhoneNumber("136........");
-        $test->setSignName("田AW");
-        $test->setTemplateCode("SMS_125015333");
-        $test->setTemplateParam(array (
-            "code" => "102530",
-        ));
-        $this->assertTrue($test->send());
-        echo $test->log;
-	}
+//	/**
+//	* A basic test example.
+//	*
+//	* @return void
+//	*/
+//	public function testx()
+//	{
+//		$test = new \Aw\Sms\Ali\Send('LTAI4********HwwtwPzTrF','Ph6JFTLC********GaGUw');
+//        $test->setPhoneNumber("136........");
+//        $test->setSignName("田AW");
+//        $test->setTemplateCode("SMS_125015333");
+//        $test->setTemplateParam(array (
+//            "code" => "102530",
+//        ));
+//        $this->assertTrue($test->send());
+//        echo $test->log;
+//	}
 
     /**
      * A basic test example.
@@ -28,8 +28,8 @@ class AliTest extends PHPUnit_Framework_TestCase
      */
     public function testb()
     {
-        $test = new \Aw\Sms\Ali\BatchSend('LTAIj*****tG','UImf5c0Pmw******WfdaIm',__DIR__."/sms.log");
-        $test->setPhoneNumber(array('136******','1*******29'));
+        $test = new \Aw\Sms\Ali\BatchSend('LTA*******G','UImf************m',__DIR__."/sms.log");
+        $test->setPhoneNumber(array('136*******','1519***********'));
         $test->setSignName(array("田AW","田AW"));
         $test->setTemplateCode("SMS_125015333");
         $test->setTemplateParam(array(
@@ -40,8 +40,9 @@ class AliTest extends PHPUnit_Framework_TestCase
                 "code" => "456",
             ),
         ));
-        $this->assertTrue($test->send());
+        $f = $test->send();
         var_dump($test->log);
+        $this->assertTrue($f);
     }
 }
 
